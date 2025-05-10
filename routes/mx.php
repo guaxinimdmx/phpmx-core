9a97bbd38f25304abf1bd9c02f8915a4503d3b87
André Ricardo <contatoadmx@gmail.com>
2025-05-10 20:21:48 -0300

    input

--- routes/mx.php
+++ routes/mx.php
@@ -3,8 +3,12 @@
 use PhpMx\Router;
 
 Router::get('', 'base.status');
+
 Router::get('favicon.ico', 'base.favicon');
 Router::get('robots.txt', 'base.robots');
 Router::get('sitemap.xml', 'base.sitemap');
+
+Router::get('captcha', 'base.captcha');
+
 Router::get('assets/...', 'base.assets');
 Router::get('download/...', 'base.download');
